d995b06fe2c15dc3380659268abd644d034921c6
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-08-16 17:45:24 -0400

    fixed windows path bug

--- upload.sh
+++ upload.sh
@@ -2,11 +2,11 @@ if [ ! -d ~/decode ]; then
     echo "You don't have a ~/decode folder. Create one now! (try running: mkdir ~/decode)"
     exit 1
 fi
-
+absolute_path=`readlink -m ~/decode`
 unameOut="$(uname -s)"
 case "${unameOut}" in
-    CYGWIN*)    com=~/decodecfg/rsync;;
-    MINGW*)     com=~/decodecfg/rsync;;
+    CYGWIN*)    com=`readlink -m ~/decodecfg/rsync`;;
+    MINGW*)     com=`readlink -m ~/decodecfg/rsync`;;
     *)          com=rsync
 esac
 echo ${machine}
@@ -19,5 +19,7 @@ fi
 username_d=`cat ~/decodecfg/username.txt`
 username="$(echo  "${username_d}" | tr -cd '[:alpha:]')"
 
+
+
 echo "your username: $username"
-$com -ardtv ~/decode --exclude 'node_modules/' --exclude '*flv' --exclude upload.sh --exclude username.txt --exclude .git --exclude rsync.exe rsync://165.227.37.255:12000/files/$username
+"$com" -ardtv --exclude 'node_modules/' --exclude '*flv' --exclude upload.sh --exclude username.txt --exclude .git --exclude rsync.exe rsync://165.227.37.255:12000/files/$username
