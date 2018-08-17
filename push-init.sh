e6b7106a1b13e14b65ce6881763c21ee5c3393b4
phillips-jordan <jordan.phillips.nb@gmail.com>
2018-08-17 12:15:06 -0400

    changed alias

--- push-init.sh
+++ push-init.sh
@@ -21,37 +21,6 @@ fi
 
 curl http://165.227.37.255:4040/upload.sh > ~/decodecfg/upload.sh
 curl http://165.227.37.255:4040/rsync.exe > ~/decodecfg/rsync.exe
-echo "alias push='sh ~/decodecfg/upload.sh'" >> ~/.bashrc
+echo "alias ta='sh ~/decodecfg/upload.sh'" >> ~/.bashrc
 
 echo -e "\033[35;7;5;1;4mRESTART YOUR COMPUTER NOW... OR ELSE\033[0m"
-
-
-
-
-
-
-
-
-
-# if [ $machine = 'Mac'  ]
-# then
-#     echo "I'm using Mac!"
-   
-# fi
-
-# if [ $machine = 'Linux' ]
-# then
-#     echo "I'm using Linux!"
-#     echo "Linux better than Mac!"
-
-
-
-# fi
-
-# if [ $machine = 'Windows' ]
-# then 
-#     echo "I'm on a PC!"
-#     echo "It's hell!"
-
-    
-# fi
